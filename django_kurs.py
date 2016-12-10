7ad77a9924f0ae7a7cc7e24c6fdb1e354730f6ba
SylviaC <s.czarnomska@gmail.com>
2016-12-10 14:17:40 +0100

    My Django Girls app, first commit

---
+++ django_kurs.py
@@ -0,0 +1,49 @@
+print("Sylwia")
+
+if 3 > 2:
+    print("To działa!")
+
+glosnosc = 57
+if glosnosc < 20:
+    print("Prawie nic nie slychac.")
+elif 20 <= glosnosc < 40:
+    print("0, muzyka leci w tle.")
+elif 40 <= glosnosc < 60:
+    print("Idealnie, mogę uslyszec wszystkie detale")
+elif 60 <= glosnosc < 80:
+    print("Dobre na imprezy")
+elif 80 <= glosnosc < 100:
+    print("Troszeczke za glosno!")
+else:
+    print("Ojoj!Moje uszy! :(")
+
+
+def hej ():
+    print("Hej!")
+    print("Jak sie masz?")
+
+hej()
+
+imie = "Sylwia"
+def hej (imie):
+    if imie == "Ola":
+        print("Hej Ola!")
+    elif imie == "Iza":
+        print("Hej Iza")
+    else:
+        print ("Hej Skarbie")
+
+hej(imie)
+
+def hej(imie):
+    print("Hej" +imie+ "!")
+
+dziewczyny = ["Sylwia", "Marta", "Justyna", "Edyta"]
+for imie in dziewczyny:
+    hej(imie)
+    print("Miło Cie widziec")
+
+
+
+
+
